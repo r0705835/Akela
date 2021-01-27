d69dcb117feef5c462ee053eadae5f60966c70c5
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-01-27 17:12:50 +0100

    Move code into src

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const messageService = require('./commands/message_replies.js');
+const { prefix } = require('../config.json');
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag);
+})
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    messageService.reply(command);
+});
+
+client.login(process.env.TOKEN);
