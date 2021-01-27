2aab5c5fc548ef06e26d0893d75c0f49fbb7349e
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-01-27 16:49:09 +0100

    Format index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const messageService = require('./message_replies.js')
 const { prefix } = require('./config.json');
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    console.log("Connected as " + client.user.tag);
 })
 
 client.on('message', message => {
@@ -14,4 +14,4 @@ client.on('message', message => {
     }
 }); 
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
