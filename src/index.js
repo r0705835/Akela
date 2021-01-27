fd60dbf9a6fb37f0438456c1ca373045390c299e
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-01-27 11:28:28 +0100

    Add reply option to messages

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const discordToken = require('../secret/discord_token.js')
+const messageService = require('./message_replies.js')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 })
+
+client.on('message', message => {
+	messageService.reply(message)
+}); 
 
 client.login(discordToken.BOT_SECRET_TOKEN)
