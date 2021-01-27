a8833a6851b95596e7c3ab5ca2098578a65bf9e5
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-01-27 17:34:54 +0100

    Refactor index to dynamically add commands

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
-const messageService = require('./commands/message_replies.js');
 const { prefix } = require('../config.json');
 const fs = require('fs');
+const path = require('path');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(path.join(__dirname, "commands")).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`);
 
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
 }
 
 
@@ -22,9 +22,16 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
+
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift().toLowerCase();
-    messageService.reply(command);
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 client.login(process.env.TOKEN);
