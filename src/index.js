533a77d3b7e1c07dfcded0dd3d4832a91000f1b9
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-01-30 17:36:49 +0100

    Add cooldown for commands

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
 
 const commandFiles = fs.readdirSync(path.join(__dirname, "commands")).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
@@ -29,6 +30,27 @@ client.on('message', message => {
     if (!client.commands.has(commandName)) return;
 
     const command = client.commands.get(commandName);
+
+    // Cooldowns from https://discordjs.guide/command-handling/adding-features.html#cooldowns
+    if(!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now ) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
         command.execute(message, args);
