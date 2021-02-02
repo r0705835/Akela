2fec331a775aca1aa840639b2a524b05708a220b
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-02-02 01:57:01 +0100

    Refactor add reaction code

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -2,6 +2,20 @@ const { roleMessage, partyGamesId, ShooterId, CrafterId, EndervilleId } = requir
 const client = require('../index.js').client;
 
 client.on('messageReactionAdd', async (reaction, user) => {
+    if (reaction.message.id !== roleMessage) return;
+
+    // When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+    }
+
     const member = reaction.message.guild.members.cache.find(member => member.id === user.id);
 
     switch (reaction.emoji.name) {
@@ -20,25 +34,4 @@ client.on('messageReactionAdd', async (reaction, user) => {
         default:
             break;
     }
-});
-
-client.on('messageReactionAdd', async (reaction, user) => {
-    if (reaction.message.id !== roleMessage) return;
-
-    // When we receive a reaction we check if the reaction is partial or not
-	if (reaction.partial) {
-		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.error('Something went wrong when fetching the message: ', error);
-			// Return as `reaction.message.author` may be undefined/null
-			return;
-		}
-    }
-
-	// Now the message has been cached and is fully available
-	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-	// The reaction is now also fully available and the properties will be reflected accurately:
-	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 });
