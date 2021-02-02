eee50d86eb318d5b6f7bd621bbe1d9fd32ecc93b
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-02-02 15:35:42 +0100

    Create text for reaction role post

---
+++ src/message.js
@@ -0,0 +1,20 @@
+const msg = (
+        "**Welcome to the PUB!**\n" +
+        "\n" +
+        "**You can opt-in (or out) for different subjects of interest**\n" +
+        "―――――――――――――――――――――――――\n" +
+        "Simply reply to this message with one of the emoji's below.\n" +
+        " 🎉: For party games like among us, jackbox, etc.\n" +
+        " 🔫: For shooter games like Valorant, Star Wars Battlefront, Borderlands, etc.\n" +
+        " ⛏️: For anything Minecraft related.\n" +
+        " 🎥: For Enderville, a full-scale minecraft video series.\n" +
+        " ☕: For the workspace, where you can work while having a casual chat.\n" +
+
+        "\n" +
+        "**Bot commando's**\n" +
+        "―――――――――――――――――――――――――\n" +
+        "Akela Bot: $help for more info.\n" +
+        "Groovy Bot: -help for more info.\n" +
+        "Rythm bot: !help for more info.\n");
+
+module.exports = { msg };
