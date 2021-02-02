bb9d20b108c3757c9f557e564a49eb7b699f5ea2
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-02-02 01:57:38 +0100

    Add remove reaction event

---
+++ src/events/messageReactionRemove.js
@@ -0,0 +1,37 @@
+const { roleMessage, partyGamesId, ShooterId, CrafterId, EndervilleId } = require('../../config.json');
+const client = require('../index.js').client;
+
+client.on('messageReactionRemove', async (reaction, user) => {
+    if (reaction.message.id !== roleMessage) return;
+
+    // When we receive a reaction we check if the reaction is partial or not
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message: ', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+		}
+    }
+
+    const member = reaction.message.guild.members.cache.find(member => member.id === user.id);
+
+    switch (reaction.emoji.name) {
+        case "🎉":
+            await member.roles.remove(partyGamesId);
+            break;
+        case "🔫":
+            await member.roles.remove(ShooterId);
+            break;
+        case "⛏️":
+            await member.roles.remove(CrafterId);
+            break;
+        case "🎥":
+            await member.roles.remove(EndervilleId);
+            break;
+        default:
+            break;
+    }
+});
