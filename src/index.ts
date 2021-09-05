ddbd0009a0dda0d2e27b8180e2fd449523950c60
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-09-05 19:36:59 +0200

    Add database support and env validation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Client } from "discord.js";
+import { connectDatabase } from "./database/connectDatabase";
+import { validateEnv } from "./utils/validateEnv";
 
 (async () => {
+    if (!validateEnv()) return;
+
     const BOT = new Client();
+    BOT.on("ready", () => console.log("Connected to Discord!"));
+    
+    await connectDatabase();
     await BOT.login(process.env.BOT_TOKEN);
 })();
