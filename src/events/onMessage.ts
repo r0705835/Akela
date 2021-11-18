07ab179cfdf9dc91d21228e5df1a5769fadb918b
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-18 09:30:45 +0100

    Implement cooldown and move message logic

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -1,14 +1,46 @@
-import { Message } from "discord.js";
+import { Message, Collection } from "discord.js";
 import { CommandList } from "../commands/_CommandsList";
+import { CommandInt } from "../interfaces/CommandInt";
+
+const prefix: string = "$";
+// Cooldowns from https://discordjs.guide/command-handling/adding-features.html#cooldowns
+const cooldowns: Collection<string, Collection<string, number>> = new Collection();
+
+function cooldown(command: CommandInt, message: Message){
+    // Cooldowns from https://discordjs.guide/command-handling/adding-features.html#cooldowns
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps: Collection<string, number> = cooldowns.get(command.name)!;
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+
+    if (timestamps.has(message.author.id)) {
+        if(!timestamps.get(message.author.id)) { return; }
+        const expirationTime = timestamps.get(message.author.id)! + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`)
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+}
+
 export const onMessage = async (message: Message) => {
-
-    const prefix = "$";
-
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
     for (const Command of CommandList) {
         if (message.content.startsWith(prefix + Command.name)) {
-            await Command.run(message);
+            try {
+                Command.run(message);
+            } catch (error) {
+                console.error(error);
+                message.reply('there was an error trying to execute that command!');
+            }
             break;
         }
     }
