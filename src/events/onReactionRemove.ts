062e84ebad63ad640c80e8a01ffe2577d3b7f601
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-19 11:13:03 +0100

    check for partials onReactionRemove

--- src/events/onReactionRemove.ts
+++ src/events/onReactionRemove.ts
@@ -1,5 +1,25 @@
 import { MessageReaction, PartialUser, User } from "discord.js";
 
 export const onReactionRemove = async (reaction: MessageReaction, user: PartialUser | User) => {
+    // When we receive a reaction we check if the reaction is partial or not
+    if (reaction.partial) {
+        // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error('Something went wrong when fetching the message: ', error);
+            // Return as `reaction.message.author` may be undefined/null
+            return;
+        }
+    }
+
+    if (user.partial) {
+        try {
+            await user.fetch();
+        } catch (error) {
+            console.error('Something went wrong when fetching the user: ', error);
+            return;
+        }
+    }
     console.log("Not implemented yet!");
 }
