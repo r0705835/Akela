01f971dba1726600d4e3cfa7a77f1c575dc99391
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-25 23:20:58 +0100

    add fields to member model

--- src/database/models/MemberModel.ts
+++ src/database/models/MemberModel.ts
@@ -1,11 +1,15 @@
 import { Document, model, Schema } from "mongoose";
 
 export interface MemberInt extends Document {
-    
+    discordId: string;
+    username: string;
+    timeLastMessage: number;
 };
 
 export const Member = new Schema({
-    
+    discordId: String,
+    username: String,
+    timeLastMessage: Number,
 });
 
 export default model<MemberInt>("member", Member);
