0249031dd9e4816f4116d3aa60d4ee2346dd4b77
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-26 12:13:42 +0100

    check memberActivity by voice activity

--- src/events/onVoiceStateUpdate.ts
+++ src/events/onVoiceStateUpdate.ts
@@ -1,5 +1,18 @@
-import { VoiceState } from "discord.js";
+import { GuildMember, VoiceState } from "discord.js";
+import MemberModel from "../database/models/MemberModel";
 
 
 export const onVoiceStateUpdate = async (oldState: VoiceState, newState: VoiceState) => {
+    try {
+        const member: GuildMember = newState.member!;
+        let targetMemberData = await MemberModel.findOne({'discordId': member.id});
+        if (!targetMemberData) {
+            targetMemberData = await MemberModel.create({
+                discordId: member.id,
+                username: member.displayName
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
