f6c0917154932fe73715a1d8a9f0455e0171de7b
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-26 14:10:25 +0100

    add guildId to member database

--- src/database/models/MemberModel.ts
+++ src/database/models/MemberModel.ts
@@ -2,11 +2,13 @@ import { Document, model, Schema } from "mongoose";
 
 export interface MemberInt extends Document {
     discordId: string;
+    guildId: string;
     username: string;
 };
 
 export const Member = new Schema({
     discordId: String,
+    guildId: String,
     username: String,
 });
 
