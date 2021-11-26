6567391baf5a0566213a91f8fb68ce61232cb879
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-26 19:39:35 +0100

    add embed code to interaction command

--- src/commands/interaction.ts
+++ src/commands/interaction.ts
@@ -1,5 +1,4 @@
 import { CommandInt } from "../interfaces/CommandInt";
-import CamperModel from "../database/models/CamperModel";
 import { MessageEmbed } from 'discord.js';
 
 export const interaction: CommandInt = {
@@ -7,6 +6,10 @@ export const interaction: CommandInt = {
     description: "A basis to experiment with interactions",
     cooldown: 3,
     run: async (message) => {
-        
+        const interactionEmbed = new MessageEmbed();
+        interactionEmbed.setTitle("Interactible embed!");
+        interactionEmbed.setDescription("Press the button to embrace the copper golem inside of you!\nWARNING: redstone contraption not provided.");
+        interactionEmbed.setColor("#71368A");
+        await message.channel.send(interactionEmbed);
     }
 }
