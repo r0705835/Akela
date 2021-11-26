f597851e1cdaf038611c651fa2d0aa383cb30a3b
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-26 15:44:42 +0100

    implement database activity in onVoiceStateUpdate

--- src/events/onVoiceStateUpdate.ts
+++ src/events/onVoiceStateUpdate.ts
@@ -1,17 +1,11 @@
 import { GuildMember, VoiceState } from "discord.js";
-import MemberModel from "../database/models/MemberModel";
+import { manageMemberData } from "../database/manageMemberData";
 
 
 export const onVoiceStateUpdate = async (oldState: VoiceState, newState: VoiceState) => {
     try {
         const member: GuildMember = newState.member!;
-        let targetMemberData = await MemberModel.findOne({'discordId': member.id});
-        if (!targetMemberData) {
-            targetMemberData = await MemberModel.create({
-                discordId: member.id,
-                username: member.displayName
-            });
-        }
+        manageMemberData(member)
     } catch (error) {
         console.error(error);
     }
