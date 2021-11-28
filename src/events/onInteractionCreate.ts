fb102b46e2720e0e1cbe82a612cb9da350796f4f
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-28 20:56:44 +0100

    create working version ping slash command

--- src/events/onInteractionCreate.ts
+++ src/events/onInteractionCreate.ts
@@ -1,6 +1,18 @@
-import { Interaction } from "discord.js";
+import { CacheType, CommandInteraction, Interaction } from "discord.js";
 
 
 export const onInteractionCreate = async (interaction: Interaction) => {
-    // TODO:
+    if(!interaction.isCommand) { 
+        return;
+    }
+
+    const command = interaction as CommandInteraction;
+
+    const { commandName, options } = command;
+
+    if (commandName === 'ping') {
+        command.reply({
+            content: 'pong',
+        })
+    }
 }
