71f32cd47edea470c67aa05505a483c61af2c2f8
r0705835 <32357167+r0705835@users.noreply.github.com>
2021-11-28 16:48:33 +0100

    temporary disable events and update client

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 require('dotenv').config();
-import { Client } from "discord.js";
+import { Client, Intents } from "discord.js";
 import { connectDatabase } from "./database/connectDatabase";
-import { onMessage } from "./events/onMessage";
+/*import { onMessage } from "./events/onMessage";
 import { onReactionAdd } from "./events/onReactionAdd";
 import { onReactionRemove } from "./events/onReactionRemove";
-import { onVoiceStateUpdate } from "./events/onVoiceStateUpdate";
+import { onVoiceStateUpdate } from "./events/onVoiceStateUpdate";*/
 import { validateEnv } from "./utils/validateEnv";
 
 (async () => {
     if (!validateEnv()) return;
 
-    const BOT = new Client();
+    const BOT = new Client({ 
+        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
     BOT.on("ready", () => console.log("Connected to Discord!"));
-    BOT.on("message", async (message) => await onMessage(message));
+    /*BOT.on("message", async (message) => await onMessage(message));
     BOT.on('messageReactionAdd', async (reaction, user) => await onReactionAdd(reaction, user));
     BOT.on('messageReactionRemove', async (reaction, user) => await onReactionRemove(reaction, user));
-    BOT.on('voiceStateUpdate', async(oldState, newState) => await onVoiceStateUpdate(oldState, newState));
+    BOT.on('voiceStateUpdate', async(oldState, newState) => await onVoiceStateUpdate(oldState, newState));*/
     await connectDatabase();
     await BOT.login(process.env.BOT_TOKEN);
 })();
